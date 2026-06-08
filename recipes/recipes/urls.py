4a5e5af1035f83c87254f6abe14be1f520563bfb
“Yurovskiy” <yurovskiy2007@gmail.com>
2026-06-08 10:14:22 +0300

    homework Django

--- recipes/recipes/urls.py
+++ recipes/recipes/urls.py
@@ -1,7 +1,7 @@
-"""first_project URL Configuration
+"""recipes URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,19 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 
-from app.views import home_view
-from app.views import time_view, workdir_view
+from django.urls import path
+from calculator import views  # импортируем views напрямую
 
 
 urlpatterns = [
-    path('', home_view, name='home'),
-    # Раскомментируйте код, чтобы данные урлы 
-    # обрабатывались Django
-    path('current_time/', time_view, name='time'),
-    path('workdir/', workdir_view, name='workdir'),
-    path('admin/', admin.site.urls),
+    path('<str:dish_name>/', views.recipe_view),  # Подключаем URLs приложения calculator
 ]
-
