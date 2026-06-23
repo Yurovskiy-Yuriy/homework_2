e136a18a6cf2adc786b9d4ee590d8f7b08993c03
“Yurovskiy” <yurovskiy2007@gmail.com>
2026-06-23 12:52:40 +0300

    add homework 2.2-databases-2

---
+++ 2_m2m-relations/articles/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from articles.models import Article
+
+
+def articles_list(request):
+    template = 'articles/news.html'
+
+    # ordering = '-published_at'
+    articles  = Article.objects.all().order_by('-published_at')
+    context = {'articles': articles}
+
+    return render(request, template, context)
